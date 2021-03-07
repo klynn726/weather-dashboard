2e81725ccd7caa9ca09154073dae72ac729ae63b
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-03-07 15:07:50 -0700

    added the first two git hub issues for enhancements

--- assets/script.js
+++ assets/script.js
@@ -3,64 +3,242 @@ function searchInput() {
   var cityName = document.getElementById('cityInput').value;
   // console.log(cityName);
 
+  //put that city into local storage
+  localStorage.setItem(cityName, cityName)
+
+//write that city on the left side
+//use bootstrap list group card  --  <li class="list-group-item">  https://getbootstrap.com/docs/4.0/components/card/
+  var historyEl = document.createElement("li");
+  historyEl.className = "list-group-item";
+  historyEl.innerHTML = localStorage.getItem(cityName);
+  document.getElementById("historyList").appendChild(historyEl);
+
+
+  document.getElementById('wxHeading').textContent = " ";
+  var now = moment().format('MM/DD/YYYY');
+  document.getElementById('wxHeading').textContent = cityName + " " + "(" + now + ")";
+
+
   //enter the city into the API
-  //see activities 1 & 2
+  //see activities 1 & 2 for guidance
 
   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=da87077027a11743c752625320934faa")
   .then(function(response){
     // console.log('first response', response)
-     //return weather data for that city 
     return response.json();
   })
-  .then(function(data) {
-    // console.log('second response', data);
 
-    //write the weather data to the right side - current
-    var now = moment().format('MM/DD/YYYY');
-    document.getElementById('#wxHeading').textContent = cityName + "(" + now + ")";
+  .then(function(data){
+
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=da87077027a11743c752625320934faa")
+
+    .then(function(response){
+      return response.json();
+    })
+
+    .then(function(data) {
+      document.getElementById('wxNow').textContent = " ";
+      document.getElementById('card1').textContent = " ";
+      document.getElementById('card2').textContent = " ";
+      document.getElementById('card3').textContent = " ";
+      document.getElementById('card4').textContent = " ";
+      document.getElementById('card5').textContent = " ";
+
+      //write the weather data to the right side - current
+      var wxBody = document.getElementById("wxNow")
+  
+      var todayIcon = document.createElement("img");
+      todayIcon.setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[0].weather.icon + ".png");
+
+      // temp
+      var tempEl = document.createElement("p");
+      tempEl.innerHTML = "Temperature: " + data.current.temp + " degrees";
+  
+      // humidity
+      var humidityEl = document.createElement("p");
+      humidityEl.innerHTML = "Humidity: " + data.current.humidity + "%";
+      // wind speed
+      var windEl = document.createElement("p");
+      windEl.innerHTML = "Wind speed: " + data.daily[0].wind_speed + " MPH";
+      // UV index
+      var uvEl = document.createElement("p");
+      uvEl.innerHTML = "UV Index: " + data.daily[0].uvi;
+
+      wxBody.appendChild(tempEl);
+      wxBody.appendChild(humidityEl);
+      wxBody.appendChild(windEl);
+      wxBody.appendChild(uvEl);
+      wxBody.appendChild(todayIcon);
+  
+      
+      //write the weather data to the lower right side - future
+
+        //day 1 card
+
+      var wxOne = document.getElementById("card1");
+      
+      var dayOne = document.createElement("div");
+      dayOne.className = "card-body";
+
+      var addOne = document.createElement("h5");
+      addOne.innerHTML = moment().add(1, 'd').format('MM/DD/YYYY');
+      addOne.className = "card-title";
+
+      //https://openweathermap.org/weather-conditions
+
+      var iconOne = document.createElement("img");
+      iconOne.setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
+     
+      var tempOne = document.createElement("p");
+      tempOne.innerHTML = "Temp: " + data.daily[1].temp.day + " deg";
+      tempOne.className = "card-text"
+
+      var humOne = document.createElement("p");
+      humOne.innerHTML = "Humidity: " + data.daily[1].humidity + "%";
+      humOne.className = "card-text";
+
+      dayOne.appendChild(addOne);
+      dayOne.appendChild(tempOne);
+      dayOne.appendChild(humOne);
+      dayOne.appendChild(iconOne);
+      wxOne.appendChild(dayOne);
+      
+
+        //day 2 card
+
+      var wxTwo = document.getElementById("card2");
+      
+      var dayTwo = document.createElement("div");
+      dayTwo.className = "card-body";
+
+      var addTwo = document.createElement("h5");
+      addTwo.innerHTML = moment().add(2, 'd').format('MM/DD/YYYY');
+
+      addTwo.className = "card-title";
+
+      iconTwo.setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png");
+
+      var tempTwo = document.createElement("p");
+      tempTwo.innerHTML = "Temp: " + data.daily[2].temp.day + " deg";
+      tempTwo.className = "card-text"
+
+      var humTwo = document.createElement("p");
+      humTwo.innerHTML = "Humidity: " + data.daily[2].humidity + "%";
+      humTwo.className = "card-text";
+
+      dayTwo.appendChild(addTwo);
+      dayTwo.appendChild(tempTwo);
+      dayTwo.appendChild(humTwo);
+      dayTwo.appendChild(iconTwo);
+      wxTwo.appendChild(dayTwo);
+
+        //day 3 card
+     
+      var wxThree = document.getElementById("card3");
+      
+      var dayThree = document.createElement("div");
+      dayThree.className = "card-body";
+
+      var addThree = document.createElement("h5");
+      addThree.innerHTML = moment().add(3, 'd').format('MM/DD/YYYY');
+
+      addThree.className = "card-title";
+
+      iconThree.setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png");
+
+      var tempThree = document.createElement("p");
+      tempThree.innerHTML = "Temp: " + data.daily[3].temp.day + " deg";
+      tempThree.className = "card-text"
+
+      var humThree = document.createElement("p");
+      humThree.innerHTML = "Humidity: " + data.daily[3].humidity + "%";
+      humThree.className = "card-text";
+
+      dayThree.appendChild(addThree);
+      dayThree.appendChild(tempThree);
+      dayThree.appendChild(humThree);
+      dayThree.appendChild(iconThree);
+      wxThree.appendChild(dayThree);
+
+        //day 4 card
+
+      var wxFour = document.getElementById("card4");
+      
+      var dayFour = document.createElement("div");
+      dayFour.className = "card-body";
+
+      var addFour = document.createElement("h5");
+      addFour.innerHTML = moment().add(4, 'd').format('MM/DD/YYYY');
+
+      addFour.className = "card-title";
+
+      iconFour.setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png");
+
+      var tempFour = document.createElement("p");
+      tempFour.innerHTML = "Temp: " + data.daily[4].temp.day + " deg";
+      tempFour.className = "card-text"
+
+      var humFour = document.createElement("p");
+      humFour.innerHTML = "Humidity: " + data.daily[4].humidity + "%";
+      humFour.className = "card-text";
+
+      dayFour.appendChild(addFour);
+      dayFour.appendChild(tempFour);
+      dayFour.appendChild(humFour);
+      dayFour.appendChild(iconFour);
+      wxFour.appendChild(dayFour);
+
+        //day 5 card
+
+      //date
+ 
+      var wxFive = document.getElementById("card5");
+      
+      var dayFive = document.createElement("div");
+      dayFive.className = "card-body";
+
+      var addFive = document.createElement("h5");
+      addFive.innerHTML = moment().add(5, 'd').format('MM/DD/YYYY');
+
+      addFive.className = "card-title";
+
+      iconFive.setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[5].weather[0].icon + ".png");
+
+      var tempFive = document.createElement("p");
+      tempFive.innerHTML = "Temp: " + data.daily[5].temp.day + " deg";
+      tempFive.className = "card-text"
+
+      var humFive = document.createElement("p");
+      humFive.innerHTML = "Humidity: " + data.daily[5].humidity + "%";
+      humFive.className = "card-text";
+
+      dayFive.appendChild(addFive);
+      dayFive.appendChild(tempFive);
+      dayFive.appendChild(humFive);
+      dayFive.appendChild(iconFive);
+      wxFive.appendChild(dayFive);
+
+  
 
 
 
 
-    
-
-  });
-
-  // .catch(function(error){
-  //   console.log(error);
-  // });
+  
+  
+    });
 
 
 
 
 
-
+  })
 
 
 
 }
 
-
-
-
-
-
-
-
-
-
-//write the weather data to the lower right side - future
-
-
-
-//put that city into local storage
-
-
-
-//write that city on the left side
-//use bootstrap list group card  --  <li class="list-group-item">
-
-//pull up the previous search from local storage 
-//when the city is clicked
 
 document.getElementById('btnSubmit').addEventListener('click', searchInput);
